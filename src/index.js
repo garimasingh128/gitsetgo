b0c917060b9984ce6b23318d99b138d78492b4b6
garimasingh128 <garingh128@gmail.com>
2020-06-23 03:03:30 +0530

    profile card added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { usePromiseTracker } from "react-promise-tracker";
+import Loader from 'react-loader-spinner';
 
+// const LoadingIndicator = props => {
+//   const { promiseInProgress } = usePromiseTracker();
+
+//   return promiseInProgress && 
+//       <h1>Hey some async call in progress ! </h1>;
+//     <div
+//       style={{
+//         width: "100%",
+//         height: "100",
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center"
+//       }}
+//     >
+//       <Loader type="Bars" color="#00BFFF" height={80} width={80} />
+//     </div>
+// };
+  
 ReactDOM.render(
   <React.StrictMode>
     <App />
