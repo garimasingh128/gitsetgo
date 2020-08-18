1b2839e2241470309bfceaf042abd9326eca33f0
divanshurox <divanshuagarwal2001@gmail.com>
2020-08-18 23:57:56 +0530

    Added loader on rendering data

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import DisplayCard from "./DisplayCard";
-
-
+import Loader from "./images/Loader.svg";
 
 const Profile = () => {
   const [data, setData] = useState({});
   const [username, setUsername] = useState("");
   const [repositories, setRepositories] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
   const onChangeHandler = (e) => {
     setUsername(e.target.value);
@@ -15,54 +15,61 @@ const Profile = () => {
   const submitHandler = async (e) => {
     e.preventDefault();
 
-    const profile = await fetch(`https://api.github.com/users/${username}`);
-    const profileJson = await profile.json();
-    console.log(profileJson);
+    try {
+      setLoading(true);
+      const profile = await fetch(`https://api.github.com/users/${username}`);
+      const profileJson = await profile.json();
+      console.log(profileJson);
 
-    const repositories = await fetch(profileJson.repos_url);
-    const repoJson = await repositories.json();
-    console.log(repoJson);
-
-    if (profileJson) {
-      setData(profileJson);
-      setRepositories(repoJson);
+      const repositories = await fetch(profileJson.repos_url);
+      const repoJson = await repositories.json();
+      console.log(repoJson);
+      if (profileJson) {
+        setData(profileJson);
+        setRepositories(repoJson);
+      }
+    } catch (err) {
+      console.log(err.message);
     }
+    setLoading(false);
   };
   return (
     <>
-      <div style={{ padding: 20 }} >
+      <div style={{ padding: 20 }}>
         <div className="ui search">
-          <div className="ui icon input">
-            <i className="search icon"></i>
-            <input
-              className="prompt"
-              placeholder="Whom you wanna stalk?"
-              type="text"
-              value={username}
-              onChange={onChangeHandler}
-            />
-          </div>
+          {isLoading ? (
+            <div style={{ marginTop: "100px" }}>
+              <object type="image/svg+xml" data={Loader} />
+            </div>
+          ) : (
+            <>
+              <div className="ui icon input">
+                <i className="search icon"></i>
+                <input
+                  className="prompt"
+                  placeholder="Whom you wanna stalk?"
+                  type="text"
+                  value={username}
+                  onChange={onChangeHandler}
+                />
+              </div>
 
-          <button
-            style={{ marginLeft: 20 }}
-            className="ui violet button"
-            type="submit"
-            onClick={submitHandler}
-          >
-            <i className="github icon"></i>
-            Go ahead!
-          </button>
-          
-          <center>
-            
-            <DisplayCard
-           
-              data={data}
-              repositories={repositories}
-            />
-          </center>
+              <button
+                style={{ marginLeft: 20 }}
+                className="ui violet button"
+                type="submit"
+                onClick={submitHandler}
+              >
+                <i className="github icon"></i>
+                Go ahead!
+              </button>
+
+              <center>
+                <DisplayCard data={data} repositories={repositories} />
+              </center>
+            </>
+          )}
         </div>
-        
       </div>
     </>
   );
