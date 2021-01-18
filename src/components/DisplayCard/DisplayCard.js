aa2e6e5dbcab862ba21f3ea994cff3d6e0cbf2d2
Thanyathon <61010497@kmitl.ac.th>
2021-01-18 15:50:33 +0700

    Move DisplayCard into folder

--- src/components/DisplayCard/DisplayCard.js
+++ src/components/DisplayCard/DisplayCard.js
@@ -1,34 +1,38 @@
-import React from "react";
-import "./css/animate.css";
-import "./css/bootstrap.min.css";
-import "./css/bootstrap.min.css.map";
+import React from 'react'
+import '../css/animate.css'
+import '../css/bootstrap.min.css'
+import '../css/bootstrap.min.css.map'
 // import "./css/fontawsom-all.min.css";
-import "./css/style.css";
-import undraw from "./images/undraw2.svg";
-import "./css/style.css.map";
+import '../css/style.css'
+import undraw from '../images/undraw2.svg'
+// import '../css/style.css.map'
+import CSSModuleMapper from '../../utils/cssModuleMapper'
+import styles from './DisplayCard.module.css'
 
-import "./js/popper.js";
+import '../js/popper.js'
 // import "./js/jquery-3.2.1.min.js";
 // import "./js/popper.min.js";
 // import "./js/bootstrap.min.js";
 // import "./js/script.js";
 
-import ReactDOM from "react-dom";
-import ReactToPdf from "react-to-pdf";
+import ReactDOM from 'react-dom'
+import ReactToPdf from 'react-to-pdf'
 
 // get our fontawesome imports
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
-import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 
-const ref = React.createRef();
+const ref = React.createRef()
 const options = {
-  unit: "cm",
-};
+  unit: 'cm',
+}
+
+const css = CSSModuleMapper(styles)
 
 const DisplayCard = ({ data, repositories }) => {
   return (
-    <div className="container-fluid overcover" style={{ marginBottom: 100 }}>
+    <div className={`container-fluid ${css('page', 'overcover')}`}>
       {data.name ? (
         <div>
           <ReactToPdf
@@ -39,8 +43,7 @@ const DisplayCard = ({ data, repositories }) => {
           >
             {({ toPdf }) => (
               <button
-                style={{ marginLeft: 20, marginTop: 20 }}
-                className="ui red button"
+                className={`ui red button ${css('gen-pdf-btn')}`}
                 type="submit"
                 onClick={toPdf}
               >
@@ -49,93 +52,93 @@ const DisplayCard = ({ data, repositories }) => {
               </button>
             )}
           </ReactToPdf>
-          <div className="container profile-box" ref={ref}>
+          <div className={css('container', 'profile-box')} ref={ref}>
             <div className="row">
-              <div className="col-md-4 left-co">
-                <div className="left-side">
-                  <div className="profile-info">
+              <div className={`col-md-4 ${css('left-co')}`}>
+                <div className={css('left-side')}>
+                  <div className={css('profile-info')}>
                     <img
                       src={data.avatar_url}
                       alt={data.avatar_url}
-                      style={{ marginTop: 30 }}
-                    ></img>{" "}
+                      className={css('avatar')}
+                    ></img>{' '}
                     <h3>{data.name}</h3>
                   </div>
-                  <h4 className="ltitle">Contact</h4>
+                  <h4 className={css('ltitle')}>Contact</h4>
 
                   {data.email ? (
-                    <div className="contact-box pb0">
-                      <div className="icon">
+                    <div className={css('contact-box', 'pb0')}>
+                      <div className={css('icon')}>
                         <i className="fas fa-phone"></i>
-                        <div className="detail">{data.email}</div>
+                        <div className={css('detail')}>{data.email}</div>
                       </div>
                     </div>
                   ) : (
                     <div> </div>
                   )}
 
-                  <div className="contact-box pb0">
-                    <div className="icon">
+                  <div className={css('contact-box', 'pb0')}>
+                    <div className={css('icon')}>
                       <FontAwesomeIcon icon={faGlobe}></FontAwesomeIcon>
                     </div>
-                    <div className="detail">{data.location}</div>
+                    <div className={css('detail')}>{data.location}</div>
                   </div>
 
-                  <h4 className="ltitle">Twitter</h4>
-                  <ul className="row social-link no-margin">
+                  <h4 className={css('ltitle')}>Twitter</h4>
+                  <ul className={`row ${css('social-link')} no-margin`}>
                     <li> @{data.twitter_username}</li>
                   </ul>
-                  <h4 className="ltitle">Organisations</h4>
+                  <h4 className={css('ltitle')}>Organisations</h4>
 
-                  <div className="refer-cov">
+                  <div className={css('refer-cov')}>
                     <b>{data.company}</b>
                   </div>
 
-                  <h4 className="ltitle">
+                  <h4 className={css('ltitle')}>
                     Followers: <i className="fas fa-pencil-alt"></i>
                     {data.followers}
                   </h4>
 
-                  <h4 className="ltitle">
+                  <h4 className={css('ltitle')}>
                     Following: <i className="fas fa-pencil-alt"></i>
                     {data.following}
                   </h4>
 
-                  <h4 className="ltitle">
-                    Profile created on: <br></br>{" "}
+                  <h4 className={css('ltitle')}>
+                    Profile created on: <br></br>{' '}
                     <i className="fas fa-pencil-alt"></i>
                     {data.created_at}
                   </h4>
 
-                  <h4 className="ltitle">
-                    Profile last updated on: <br></br>{" "}
+                  <h4 className={css('ltitle')}>
+                    Profile last updated on: <br></br>{' '}
                     <i className="fas fa-pencil-alt"></i>
                     {data.updated_at}
                   </h4>
                 </div>
               </div>
               <div className="col-md-8 rt-div">
-                <div className="rit-cover">
-                  <div className="hotkey">
+                <div className={css('rit-cover')}>
+                  <div className={css('hotkey')}>
                     <h1 className="">{data.name}</h1>
                     <small>{data.login}</small>
                   </div>
-                  <h2 className="rit-titl">
+                  <h2 className={css('rit-titl')}>
                     <i className="far fa-user"></i>About
                   </h2>
-                  <div className="about">
+                  <div className={css('about')}>
                     <p>{data.bio}</p>
                   </div>
-                  <h2 className="rit-titl">
-                    <i className="far fa-user"></i>Public Repositories:{" "}
-                    {data.public_repos}{" "}
+                  <h2 className={css('rit-titl')}>
+                    <i className="far fa-user"></i>Public Repositories:{' '}
+                    {data.public_repos}{' '}
                   </h2>
-                  <h2 className="rit-titl">
-                    <i className="far fa-user"></i>Public Gists:{" "}
-                    {data.public_gists}{" "}
+                  <h2 className={css('rit-titl')}>
+                    <i className="far fa-user"></i>Public Gists:{' '}
+                    {data.public_gists}{' '}
                   </h2>
 
-                  <h2 className="rit-titl">
+                  <h2 className={css('rit-titl')}>
                     <i className="fas fa-briefcase"></i>Repositories
                   </h2>
 
@@ -148,13 +151,17 @@ const DisplayCard = ({ data, repositories }) => {
                           key={repo.name}
                         >
                           <div classNameName="item">
-                            <i classNameName="large github middle aligned icon"></i>
+                            <i
+                              classNameName={`large github middle aligned ${css(
+                                'icon'
+                              )}`}
+                            ></i>
                             <div classNameName="content">
                               <ul>
                                 <li>
                                   <a
                                     href={repo.html_url}
-                                    classNameName="header"
+                                    classNameName={css('header')}
                                     target="_blank"
                                   >
                                     {repo.name}
@@ -168,10 +175,10 @@ const DisplayCard = ({ data, repositories }) => {
                     </a>
                   </div>
 
-                  <h2 className="rit-titl">
+                  <h2 className={css('rit-titl')}>
                     <i className="fas fa-graduation-cap"></i>Important Links
                   </h2>
-                  <div className="education">
+                  <div className={css('education')}>
                     <ul className="row no-margin">
                       <li className="col-md-6">
                         <span>
@@ -195,14 +202,17 @@ const DisplayCard = ({ data, repositories }) => {
           </div>
         </div>
       ) : (
-        <div >
-          <img src={undraw} style={{width: 1500, height: 400, marginTop: 40}}></img>
-          </div>
+        <div>
+          <img
+            src={undraw}
+            style={{ width: 1500, height: 400, marginTop: 40 }}
+          ></img>
+        </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default DisplayCard;
+export default DisplayCard
 
 //https://api.github.com/users/garimasingh128/repos
---
+++ src/utils/cssModuleMapper.js
@@ -0,0 +1,29 @@
+/**
+ * @typedef {{[key:string] : string}} CSSModule
+ */
+
+/**
+ * Function for join multiple CSS Modules classes
+ *
+ * @param {CSSModule} styles Default export from '*.module.css' files
+ *
+ * @example [ How to use ]
+ * import CSSModuleMapper from './path/to/this/file'
+ * import styles from '*.module.css'
+ *
+ * const css = CSSModuleMapper(styles)
+ * // use in JSX
+ * <div className={css('foo', 'bar')}>Test</div>
+ */
+const CSSModuleMapper = (styles) =>
+  /**
+   * @param {string[]} classesName Array of class name
+   */
+  (...classesName) => {
+    return classesName
+      .map((name) => styles[name])
+      .filter((name) => name !== '')
+      .join(' ')
+  }
+
+export default CSSModuleMapper
